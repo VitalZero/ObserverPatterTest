ea165c5548e5a87f337bd3ab1a8e0117c6338b08
VitalZero <46072303+VitalZero@users.noreply.github.com>
2023-02-24 00:06:17 -0600

    Added JeffM's event handler (discord)
    Thx Jeff!

--- main.cpp
+++ main.cpp
@@ -1,3 +1,96 @@
+//#pragma once
+//
+//#include <vector>
+//#include <memory>
+//
+//class CancelationToken
+//{
+//public:
+//    inline void Release() { Active = false; }
+//    inline bool Valid()const  { return Active; }
+//
+//    using Ptr = std::shared_ptr<CancelationToken>;
+//protected:
+//    bool Active = true;
+//};
+//
+//class CancelationTokenHost
+//{
+//public:
+//    CancelationTokenHost()
+//    {
+//        TokenPtr = std::make_shared<CancelationToken>();
+//    }
+//
+//    virtual ~CancelationTokenHost()
+//    {
+//        TokenPtr->Release();
+//    }
+//
+//    CancelationToken::Ptr Get() const { return TokenPtr; }
+//
+//private:
+//    CancelationToken::Ptr TokenPtr;
+//};
+//
+//template<typename F>
+//class EventHandler
+//{
+//private:
+//    std::vector<std::pair<F,CancelationToken::Ptr>> Callbacks;
+//
+//public:
+//    inline void Add(F callback, CancelationToken::Ptr token = nullptr)
+//    {
+//        Callbacks.push_back(std::pair<F, CancelationToken::Ptr>{callback, token});
+//    }
+//
+//    inline void Add(F callback, const CancelationTokenHost& token)
+//    {
+//        Callbacks.push_back(std::pair<F, CancelationToken::Ptr>{callback, token.Get()});
+//    }
+//
+//    inline void Remove(F callback)
+//    {
+////         std::vector<std::pair<F, CancelationToken::Ptr>>::iterator itr = Callbacks.begin();
+////         while (itr != Callbacks.end())
+////         {
+////             if (itr->first == callback)
+////                 itr = Callbacks.erase(itr);
+////             else
+////                 itr++;
+////         }
+//    }
+//
+//    template<typename ...Args>
+//    inline void Call(Args&&... args)
+//    {
+//        auto itr = Callbacks.begin();
+//        while (itr != Callbacks.end())
+//        {
+//            bool valid = !itr->second || itr->second->Valid();
+//
+//            if (!valid)
+//            {
+//                itr = Callbacks.erase(itr);
+//            }
+//            else
+//            {
+//                itr->first(std::forward<Args>(args)...);
+//                itr++;
+//            }
+//        }
+//    }
+//};
+//
+//class Player
+//{
+//  public:
+//    EventHandler<std::function<void(int)>> ScoreChanged;
+//};
+//
+//player.ScoreChange.Add([](int score){handleScoreChange(score);});
+
 #include <raylib.h>
 #include "Observer_Subject.h"
 #include <string>
